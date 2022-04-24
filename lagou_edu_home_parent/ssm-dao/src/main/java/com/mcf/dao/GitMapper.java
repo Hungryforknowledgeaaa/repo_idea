e9d01ae7596a587d3fc617b7c50310bfab59e63f
MorningCoffee <MorningCoffee@xxx.com>
2022-04-25 00:23:53 +0800

    提交 GitMapper.java

--- lagou_edu_home_parent/ssm-dao/src/main/java/com/mcf/dao/GitMapper.java
+++ lagou_edu_home_parent/ssm-dao/src/main/java/com/mcf/dao/GitMapper.java
@@ -13,5 +13,11 @@ public interface GitMapper {
 
     List<User> selectAllUser();
 
-    //s
+    void test01();
+    void test02();
+    void test03();
+    void test04();
+    void test05();
+    void test06();
+    void test07();
 }
